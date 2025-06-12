a2ecb25420339fcce7844eabd968f114053c4264
chendicong <chendc_work@163.com>
2025-06-12 18:15:22 +0800

    feat: 添加拦截器、过滤器

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './transform/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3005);
+  
+  app.setGlobalPrefix('api') // 路由全局前缀
+
+  app.useGlobalInterceptors(new TransformInterceptor()) // 全局拦截器
+
+  await app.listen(process.env.PORT ?? 3005); // 端口
 }
 bootstrap();
---
+++ src/transform/transform.interceptor.ts
@@ -0,0 +1,22 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { error } from 'console';
+import { Observable, map } from 'rxjs';
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        return {
+          error: 0,
+          data,
+        };
+      }),
+    );
+  }
+}
