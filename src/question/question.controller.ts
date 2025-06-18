42cc8f3b1701f2538436405be2a77834afcbc2c8
chendicong <chendc_work@163.com>
2025-06-18 16:11:46 +0800

    feat: 添加mongodb搜索和分页

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -7,13 +7,59 @@ import {
   Body,
   HttpException,
   HttpStatus,
+  Post,
+  Delete,
 } from '@nestjs/common';
 import { QuestionDto } from './dto/question.dto'
+import { QuestionService } from './question.service';
 
 @Controller('question')
 export class QuestionController {
-    @Get('test')
-    getTest(): string {
-        throw new HttpException('获取数据失败', HttpStatus.BAD_REQUEST)
+  // 依赖注入
+  constructor(private readonly questionSerive: QuestionService) {}
+  @Get('test')
+  getTest(): string {
+      throw new HttpException('获取数据失败', HttpStatus.BAD_REQUEST)
+  }
+
+  @Post()
+  create() {
+    return this.questionSerive.create();
+  }
+
+  @Get()
+  async findAll(
+    @Query('keyword') keyword: string,
+    @Query('page') page: number,
+    @Query('pageSize') pageSize: number
+  ) {
+
+    const list = await this.questionSerive.findAllList({
+      keyword,
+      page,
+      pageSize,
+    });
+
+    const count = await this.questionSerive.countAll({ keyword })
+
+    return {
+      list,
+      count
     }
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.questionSerive.findOne(id);
+  }
+
+  @Patch(':id')
+  updateOne(@Param('id') id: string, @Body() questionDto: QuestionDto) {
+    return this.questionSerive.update(id, questionDto);
+  }
+
+  @Delete(':id')
+  deleteOne(@Param('id') id: string) {
+    return this.questionSerive.delete(id);
+  }
 }
--- src/question/question.service.ts
+++ src/question/question.service.ts
@@ -1,4 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Question } from './schemas/question.schema';
 
 @Injectable()
-export class QuestionService {}
+export class QuestionService {
+    constructor(
+        // 依赖注入
+        @InjectModel(Question.name) private readonly questionModel,
+    ) {}
+
+    // 创建
+    async create() {
+        const question = new this.questionModel({
+            title: 'title' + Date.now(),
+            desc: 'desc'
+        })
+
+        return await question.save()
+    }
+
+    // 删除
+    async delete(id: string) {
+        return await this.questionModel.findByIdAndDelete(id)
+    }
+
+    // 更新
+    async update(id: string, updateData) {
+        return await this.questionModel.updateOne({ _id: id }, updateData)
+    }
+
+    // 查找
+    async findOne(id: string) {
+        return await this.questionModel.findById(id)
+    }
+
+    async findAllList({ keyword = '', page = 1, pageSize = 10 }) {
+        const whereOpt: any = {}
+        if (keyword) {
+            const reg = new RegExp(keyword, 'i');
+            whereOpt.title = { $regex: reg } // 模糊搜索
+        }
+        return await this.questionModel
+            .find(whereOpt)
+            .sort({ _id: -1 }) // 逆序排序
+            .skip((page -1) * pageSize) // 分页
+            .limit(pageSize)
+    }
+
+    async countAll({ keyword = '' }) {
+        const whereOpt: any = {}
+        if (keyword) {
+            const reg = new RegExp(keyword, 'i');
+            whereOpt.title = { $regex: reg } // 模糊搜索
+        }
+        return await this.questionModel.countDocuments(whereOpt)
+    }
+}
