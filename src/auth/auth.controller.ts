46155c0bd5eb85e6190cdd63be7b12a2fde8f5b7
chendicong <chendc_work@163.com>
2025-06-19 18:05:52 +0800

    feat: 增加pulic校验

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Post, Get, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
-import { AuthGuard } from './auth.guard'
+import { AuthGuard } from './auth.guard';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
+    @Public()
     @Post('login')
     async login(@Body() userInfo: CreateUserDto) {
         const { username, password } = userInfo;
--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -2,12 +2,25 @@ import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from
 import { JwtService } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 import { Request } from 'express';
+import { IS_PUBLIC_KEY } from './decorators/public.decorator';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+        context.getHandler(),
+        context.getClass(),
+    ]);
+    if (isPublic) {
+        return true
+    }
+
     const request = context.switchToHttp().getRequest();
     const token = this.extrackTokenFromHeader(request);
     if (!token) {
